0da0887a22a808b8950e8b5c12cba7fc240dad58
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-07 18:12:51 -0800

    fix Apple Silicon

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,5 +10,4 @@ cargo-bundle-licenses \
     --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
-export JEMALLOC_OVERRIDE=${PREFIX}/lib/libjemalloc${SHLIB_EXT}
 cargo install --bins --no-track --locked --root "${PREFIX}" --path .
